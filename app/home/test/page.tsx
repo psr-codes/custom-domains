8b72741b450dd0f0574ac4714d7e9aec49f12602
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2025-03-24 22:40:27 +0530

    fixed dashboard test page.tsx

--- app/home/test/page.tsx
+++ app/home/test/page.tsx
@@ -1,26 +1,26 @@
 import prisma from "@/lib/prisma";
 import A from "@/components/templates/test-templates/A";
 import B from "@/components/templates/test-templates/B";
+
 export default async function SitePage({
     params,
 }: {
-    params: { subdomain: string };
+    params: Promise<{ subdomain: string }>;
 }) {
-    const { subdomain } = params;
+    const { subdomain } = await params;
 
-    // Fetch site data from the database
+    // Fetch site data from the database (uncomment and adjust as needed)
     // const site = await prisma.site.findUnique({
-    //     where: { subdomain },
+    //   where: { subdomain },
     // });
 
     // if (!site) {
-    //     return <div>{subdomain} Site not found</div>;
+    //   return <div>{subdomain} Site not found</div>;
     // }
 
     return (
-        <div className="bg-white text-red-500  rounded-lg shadow overflow-clipped ">
+        <div className="bg-white text-red-500 rounded-lg shadow overflow-hidden">
             <h1>{subdomain}</h1>
-
             <A subdomain={subdomain} />
         </div>
     );
